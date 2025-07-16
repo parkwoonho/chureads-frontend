78f895e950a7a83fe7639836c1056e450927f7e9
parkwoonho <pwh3@naver.com>
2025-07-16 16:42:11 +0900

    last commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../components/InputField";
 import LoginButton from "../components/LoginButton";
 import { Link, useNavigate } from "react-router-dom";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
 
 const Login = () => {
-  // logic
-  const history = useNavigate()
+// logic
+const history = useNavigate();
 
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
+// 로딩 상태
+const [isLoading, setIsLoading] = useState(false);
+const [errorMessage, setErrorMessage] = useState("");
 
-  const handleInputChange = (inputValue, field) => {
-    // TODO: 사용자 입력 기능 구현
-    console.log("사용자 입력 기능 구현 ");
-  };
+const handleInputChange = (inputValue, field) => {
+  if (field === "email") {
+    setEmail(inputValue);
+  } else {
+    setPassword(inputValue);
+  }
+};
 
-  const handleLogin = (event) => {
-    // TODO: 로그인 기능 구현
-    console.log("로그인 ");
-  };
+const handleLogin = async (event) => {
+  event.preventDefault(); // 폼 제출시 새로고침 방지 메소드
+  // 로그인 기능
+  setErrorMessage("");
+
+  // 로딩중이거나 사용자가 emaill, password값 작성 안하면 실행안함
+  if (isLoading || !email || !password) return;
+  console.log("email", email);
+  console.log("password", password);
+
+  setIsLoading(true);
+  try {
+    // 비동기처리 성공시
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log("🚀 ~ handleLogin ~ userCredential:", userCredential);
+
+    // 홈화면으로 리다이렉트
+    history("/");
+  } catch (error) {
+    // 비동기처리 실패시
+    setErrorMessage(error.message);
+  } finally {
+    // 성공, 실패 상관없이 마지막에 실행
+    setIsLoading(false);
+  }
+};
 
   const handleGoogleLogin = async () => {
     // TODO: 구글 로그인 구현
@@ -59,6 +93,9 @@ const Login = () => {
             field="password"
             onChange={handleInputChange}
           />
+          {errorMessage &&<p className="text-red-600">
+          {errorMessage}</p>}
+
           <LoginButton category="login" text="Login" />
         </form>
         {/* END: 폼 영역 */}
